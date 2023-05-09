edbf29234dafbc087c9ef7c1910b54e53c21853a
sebillac81 <sebillac@hotmail.com>
2023-05-09 13:09:02 -0300

    deployment update

--- scripts/Deployment.ts
+++ scripts/Deployment.ts
@@ -4,14 +4,23 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const ADDRESS = "0xc9d59a4D2d1FA4b4F3496b8d08EF43A122C89FBc";
+
 async function main() {
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "");
-    const provider = new ethers.providers.AlchemyProvider(
-        "goerli",
-        process.env.ALCHEMY_API_KEY
+    const provider = new ethers.providers.InfuraProvider(
+        "sepolia",
+        process.env.INFURA_API_SECRET
     );
 
+    const lastBlock = await  provider?.getBlock("latest");
+    console.log(`Connected to the blocknumber ${lastBlock?.number}`)
+
     const signer = wallet.connect(provider);
+
+    const price = await provider.getGasPrice();
+
+    console.log(`Gas price is ${price}`);
+
     const balance = await signer.getBalance();
     console.log(`Balance is ${balance} WEI`);
 
@@ -23,6 +32,7 @@ async function main() {
         proposal.map(ethers.utils.formatBytes32String));
 
     const deployTxReceipt = ballotContract.deployTransaction.wait();
+    console.log("The contract was deployed");
     
     const chairPerson = await ballotContract.chairperson();
     console.log(`The chairperson is: ${chairPerson}`);
